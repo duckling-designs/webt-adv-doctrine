bb5d4c90e1d90d76e9063a348f85e61d623a2d64
Raven Burkard <mathematti@outlook.com>
2024-04-23 08:46:22 +0200

    new database yay

--- rps.sql
+++ rps.sql
@@ -1,3 +1,4 @@
+drop database if exists rps_db;
 create database if not exists rps_db;
 use rps_db;
 
@@ -7,51 +8,38 @@ create table if not exists player
     username varchar(255) not null
 );
 
-create table if not exists symbol
+create table if not exists symbols
 (
-    pk_id        int primary key auto_increment,
-    symbol_name  varchar(255) not null,
-    fk_game_id   int          not null,
-    fk_player_id int          not null
+    pk_id   int primary key auto_increment,
+    symbol  varchar(255) not null
 );
 
-create table if not exists games
+create table if not exists rounds
 (
-    pk_id          int primary key auto_increment,
-    date_played    datetime not null
+    pk_id       int primary key auto_increment,
+    date_played datetime not null default current_timestamp,
+    fk_player_1 int not null,
+    fk_player_2 int not null,
+    fk_player_1_symbol int not null,
+    fk_player_2_symbol int not null,
+    constraint fk_rounds_player_1 foreign key (fk_player_1) references player (pk_id),
+    constraint fk_rounds_player_2 foreign key (fk_player_2) references player (pk_id),
+    constraint fk_rounds_player_1_symbol foreign key (fk_player_1_symbol) references symbols (pk_id),
+    constraint fk_rounds_player_2_symbol foreign key (fk_player_2_symbol) references symbols (pk_id)
 );
+
+insert into symbols (symbol) values ('rock');
+insert into symbols (symbol) values ('paper');
+insert into symbols (symbol) values ('scissors');
 
 insert into player (username) values ('Raven');
 insert into player (username) values ('Lisa');
+insert into player (username) values ('Sophie');
 insert into player (username) values ('Roman');
 insert into player (username) values ('Severin');
 
-insert into games (date_played) values (sysdate());
-insert into games (date_played) values (sysdate());
-insert into games (date_played) values (sysdate());
-insert into games (date_played) values (sysdate());
-insert into games (date_played) values (sysdate());
-insert into games (date_played) values (sysdate());
-
-insert into symbol (symbol_name, fk_game_id, fk_player_id) values ('rock', 1, 1);
-insert into symbol (symbol_name, fk_game_id, fk_player_id) values ('scissors', 1, 2);
-
-insert into symbol (symbol_name, fk_game_id, fk_player_id) values ('paper', 2, 3);
-insert into symbol (symbol_name, fk_game_id, fk_player_id) values ('rock', 2, 4);
-
-insert into symbol (symbol_name, fk_game_id, fk_player_id) values ('scissors', 3, 1);
-insert into symbol (symbol_name, fk_game_id, fk_player_id) values ('rock', 3, 3);
-
-insert into symbol (symbol_name, fk_game_id, fk_player_id) values ('rock', 4, 2);
-insert into symbol (symbol_name, fk_game_id, fk_player_id) values ('scissors', 4, 4);
-
-insert into symbol (symbol_name, fk_game_id, fk_player_id) values ('paper', 5, 1);
-insert into symbol (symbol_name, fk_game_id, fk_player_id) values ('rock', 5, 4);
-
-insert into symbol (symbol_name, fk_game_id, fk_player_id) values ('scissors', 6, 2);
-insert into symbol (symbol_name, fk_game_id, fk_player_id) values ('rock', 6, 3);
-
-select * from games g
-         join symbol s on g.pk_id = s.fk_game_id
-         join player p on s.fk_player_id = p.pk_id
-order by g.pk_id;
+insert into rounds (date_played, fk_player_1, fk_player_2, fk_player_1_symbol, fk_player_2_symbol) values ('2024-04-16 10:00:00', 1, 2, 1, 2);
+insert into rounds (date_played, fk_player_1, fk_player_2, fk_player_1_symbol, fk_player_2_symbol) values ('2024-04-16 11:00:00', 3, 4, 3, 2);
+insert into rounds (date_played, fk_player_1, fk_player_2, fk_player_1_symbol, fk_player_2_symbol) values ('2024-04-16 12:00:00', 5, 1, 1, 3);
+insert into rounds (date_played, fk_player_1, fk_player_2, fk_player_1_symbol, fk_player_2_symbol) values ('2024-04-16 13:00:00', 2, 3, 2, 3);
+insert into rounds (date_played, fk_player_1, fk_player_2, fk_player_1_symbol, fk_player_2_symbol) values ('2024-04-16 14:00:00', 4, 5, 1, 1);
